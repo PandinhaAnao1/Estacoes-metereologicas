1b20b6e7b00db912c09522524a2ee114668b2076
Kelve Oliveira <kelveoliveira052@gmail.com>
2024-09-16 19:57:26 -0400

    rota de alualizar usuário concertada

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -18,7 +18,7 @@ class Usuario {
 
   static atualizar = async (req, res) => {
     try {
-      const id = { id: req.params.id };
+      const id = {id: req.params.id};
       const { nome, email, senha } = req.body;
       const data = { nome, email, senha };
       const response = await UsuarioService.atualizar(id, data);
@@ -29,7 +29,11 @@ class Usuario {
         message: "Usuario atualizado com sucesso.",
       });
     } catch (error) {
-      return res.status(error.code || 500).json(error);
+      return res.status(error.code || 500).json({
+        error: false,
+        code: error.code || 500,
+        message: error.message,
+      });
     };
   };
 
