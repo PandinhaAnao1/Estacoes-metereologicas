85c1bb71b125c49c53ede935a10fced6a51a5bee
Nessa515 <vanessaoliveiradasilva515@gmail.com>
2024-10-01 21:39:29 -0400

    Feito a revisão da rota get de usuarios

---
+++ src/schemas/usuarioSchema.js
@@ -0,0 +1,23 @@
+import {z} from "zod";
+
+class UsuarioSchema{
+    static listarUsuario = z.object({
+        id: z.preprocess((val) => Number(val), z.number({
+            invalid_type_error: "Id informado não é do tipo number.",
+        }).int({
+            message: "Id informado não é um número inteiro."
+        }).positive({
+            message: "Id informado não é positivo."
+        })).optional(),
+        nome: z.string({
+            invalid_type_error: "Nome informado não é do tipo string."
+        }).trim().optional(),
+        email: z.string({
+            invalid_type_error: "Email informado deve ser do tipo string."
+        }).email({
+            message: "Email invalido."
+        }).optional(),
+    })
+}
+
+export default UsuarioSchema;
