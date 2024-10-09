c44481c27e0c282e4c67c661a40bc6c8ab629913
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2024-10-09 17:36:18 -0400

    Conclui os teste de usuario especificament o teste de midleware de autenticação

--- src/tests/unitario/autenticacaoUnidade.spec.js
+++ src/tests/unitario/autenticacaoUnidade.spec.js
@@ -1,72 +1,35 @@
-import AutenticacaoServices from '../../services/autenticacaoSevices.js';
-import UsuarioRepository from '../../repositories/usuarioRepository.js';
-import Hashsenha from '../../util/hashSenha.js';
+import authentication from '../../middleware/authenticationUser.js';
 import Jwt from 'jsonwebtoken';
-import { z } from 'zod';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-jest.mock('jsonwebtoken', () => ({
-  sign: jest.fn(),
-}));
+describe('Teste para verificar o middleware de autenticação', () => {
+  let req, res, next;
 
-jest.mock('../../repositories/usuarioRepository.js', () => ({
-  findMany: jest.fn(),
-}));
-
-jest.mock('../../util/hashSenha.js', () => ({
-  compararSenha: jest.fn(),
-}));
-
-describe('AutenticacaoServices', () => {
   beforeEach(() => {
-    jest.clearAllMocks();
+    req = {
+      headers: {
+        authorization: 'Bearer token'
+      }
+    };
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    next = jest.fn();
   });
 
-  describe('Deve autenticar e criar token de um usuario', () => {
-    it('Deve realizar log com sucesso', () => {
-      let email = "emailTeste@gmail.com";
-      let senha = 'SenhaForte123';
-
-      //Retiarar senha do token
-      let tokenTest = Jwt.sign({ email, senha }, process.env.JWT_SECRET, { expiresIn: '30d' });
-
-      UsuarioRepository.findMany.mockResolvedValueU([{
-        id: 1,
-        nome: 'Usuario teste de unidade',
-        email: email
-      }]);
-
-      const resultado = AutenticacaoServices.login(email);
-      expect(token).toBe('token_mock');
+  it('Teste para cobrir o erro 401', () => {
+    jest.spyOn(Jwt, 'verify').mockImplementation(() => {
+      throw new Error('Token invalido!');
     });
 
-    it('deve lançar erro se os dados forem inválidos', async () => {
-      const data = { email: 'invalido', senha: '123' };
+    authentication(req, res, next);
+    console.log(res);
 
-      await expect(AutenticacaoServices.criarToken(data))
-        .rejects
-        .toEqual(expect.objectContaining({
-          message: expect.any(Array),
-          code: 400,
-          error: true,
-        }));
-    });
 
-    it('deve lançar erro se a senha for inválida', async () => {
-      const data = { email: 'teste@example.com', senha: 'Senha123!' };
-      const usuarioMock = [{ email: 'teste@example.com', senha: 'hash' }];
-      UsuarioRepository.findMany.mockResolvedValue(usuarioMock);
-      Hashsenha.compararSenha.mockResolvedValue(false);
-
-      await expect(AutenticacaoServices.criarToken(data))
-        .rejects
-        .toEqual({
-          message: "Senha Invalida!",
-          code: 400,
-          error: true,
-        });
-    });
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(next).not.toHaveBeenCalled();
   });
 });
