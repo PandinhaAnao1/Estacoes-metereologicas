9c101cabf6d3e2e0aa9774a4ded40ec0fdf40130
Caio <caioosalencar@gmail.com>
2024-10-19 14:46:36 -0400

    Revisando Get ListarPorID Breach 96

---
+++ src/docs/schemas/usuarios/usuarioListarID.js
@@ -0,0 +1,59 @@
+const ListarPorIDSchemas = {
+    listar_ID_200:{
+        type: "object",
+        properties: {
+          
+            data: {
+            type: "object", 
+            items: {}, 
+            example: {
+
+              data: {
+                type: 'array', example: [
+                  {
+                    "id": 1,
+                    "nome": "usuario Atualizado",
+                    "email": "vitorgabriel123@gmail.com"
+                  }
+                ]
+              },
+              error: { type: "boolean", example: false },
+              code: { type: "int", example: 200 },
+              message: { type: "string", example: "Usuário encontrado com sucesso" }
+            }
+          },
+        }
+      
+    },
+
+    listar_ID_404:{
+        
+        type: "object",
+        properties: {
+          data: {
+            type: "object", items: {}, example:
+            {
+              error: { type: "boolean", example: true },
+              code: { type: "int", example: 404 },
+              message: { type: "array", example: ["ID invalido"] }
+            }
+          },
+        }
+      
+    },
+
+    listar_ID_400:{
+        
+        type: "object",
+        properties: {
+          data: {
+            type: "object", items: {}, example:
+            {
+              error: { type: "boolean", example: true },
+              code: { type: "int", example: 400 },
+              message: { type: "array", example: ["Usuário não encontrado"] }
+            }
+          },
+        }
+    }
+}
