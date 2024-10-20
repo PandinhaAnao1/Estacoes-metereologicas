a1a83f44c99b16385018761d22ca75d72855042a
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2024-10-20 17:30:36 -0400

    Adicionei o before each no teste de controller estações

--- src/tests/unitario/controller/estacaoController.spec.js
+++ src/tests/unitario/controller/estacaoController.spec.js
@@ -1,5 +1,5 @@
 import EstacaoController from '../../../controllers/estacaoController.js';
-import {describe, expect} from '@jest/globals';
+import { describe, expect } from '@jest/globals';
 
 jest.mock('../../../services/usuarioService', () => ({
     listar: jest.fn().mockRejectedValue(new Error('Erro interno do serviço')),
@@ -12,13 +12,26 @@ beforeEach(() => {
     jest.clearAllMocks();
 });
 
-describe('Testando o usuario controller', () => {
-    
+describe('Teste que verifica o controller de estações', () => {
+    let req;
+    let res;
+
+    beforeEach(() => {
+        req = {
+            body: {}
+        };
+
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+    });
+
     it('Deve retornar erro 500 ao listar estações', async () => {
         const sendErrorMock = jest.fn();
         const res = { status: jest.fn(() => ({ json: sendErrorMock })) };
 
-        const req = {body: null};
+        const req = { body: null };
 
         await EstacaoController.listar(req, res);
 
@@ -29,7 +42,7 @@ describe('Testando o usuario controller', () => {
         const sendErrorMock = jest.fn();
         const res = { status: jest.fn(() => ({ json: sendErrorMock })) };
 
-        const req = {body: null};
+        const req = { body: null };
 
         await EstacaoController.listarPorId(req, res);
 
@@ -40,7 +53,7 @@ describe('Testando o usuario controller', () => {
         const sendErrorMock = jest.fn();
         const res = { status: jest.fn(() => ({ json: sendErrorMock })) };
 
-        const req = {body: null};
+        const req = { body: null };
 
         await EstacaoController.cadastrar(req, res);
 
@@ -51,7 +64,7 @@ describe('Testando o usuario controller', () => {
         const sendErrorMock = jest.fn();
         const res = { status: jest.fn(() => ({ json: sendErrorMock })) };
 
-        const req = {body: null};
+        const req = { body: null };
 
         await EstacaoController.atualizar(req, res);
 
