81039d210a156baab9ecd6ae6d31865c6399d62d
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2024-11-05 07:49:04 -0400

    Corrigir a documentação do post de dados, usei o comunResponse e mudei o schema do docs

--- src/docs/schemas/dados/dadosCadastrar.js
+++ src/docs/schemas/dados/dadosCadastrar.js
@@ -1,21 +1,13 @@
 const dadosSchema = {
     dados_cadastrar_201: {
         type: "object",
-        properties: {
-            data: {
-                type: "object",
-                example: {
-                    id: 1,
-                    temperature: 22.5,
-                    humidity: 75.0,
-                    rainfall: 5.2,
-                    wind_speed_kmh: 15.0,
-                    data_hora: "2024-08-23T12:34:56Z"
-                }
-            },
-            error: { type: "boolean", example: false },
-            code: { type: "int", example: 201 },
-            message: { type: "string", example: "Dados salvos com sucesso!" }
+        example: {
+            id: 1,
+            temperature: 22.5,
+            humidity: 75.0,
+            rainfall: 5.2,
+            wind_speed_kmh: 15.0,
+            data_hora: "2024-08-23T12:34:56Z"
         }
     }
 }
