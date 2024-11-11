b9b9466a5ca4bfe6dee08a4f87f55ee9f15661e2
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2024-11-11 18:15:28 -0400

    Corrigir o controller de listar estações, coloquei o tratamento de erros no catch mudei o req.query para os listar direto

--- src/controllers/estacaoController.js
+++ src/controllers/estacaoController.js
@@ -1,7 +1,7 @@
 import env from "dotenv";
 import EstacaoService from "../services/estacaoService.js";
 import { sendError, sendResponse } from "../util/messages.js";
-import  { z } from 'zod';
+import { z } from 'zod';
 import { APIErro } from "../util/apiErrro.js";
 
 env.config();
@@ -9,18 +9,7 @@ env.config();
 class Estacao {
   static listar = async (req, res) => {
     try {
-      const { id, nome, endereco, latitude, longitude, ip, status, usuario_id } = req.query;
-      const filtro = {
-        id: id,
-        nome: nome,
-        endereco: endereco,
-        latitude: latitude,
-        longitude: longitude,
-        ip: ip,
-        status: status,
-        usuario_id: usuario_id
-      };
-      const response = await EstacaoService.listar(filtro);
+      const response = await EstacaoService.listar(req.query);
       res.status(200).json({
         data: response,
         error: false,
@@ -28,40 +17,56 @@ class Estacao {
         message: response.length > 1 ? "Estações encontradas com sucesso." : "Estação encontrada com sucesso.",
       });
     } catch (error) {
-      return res.status(error.code || 500).json(error);
+      if (error instanceof APIErro) {
+        const { code, errors } = error.toJson();
+        return sendError(res, code, ...errors);
+      }
+
+      if (error instanceof z.ZodError) {
+        let errors = [];
+        error.issues.map((issue) => (
+          errors.push({
+            path: issue.path[0],
+            message: issue.message
+          })
+        ));
+        return sendError(res, 400, errors);
+      }
+
+      sendError(res, 500, []);
     };
   };
 
   // GET por ID - listar Usuario por ID 
   static listarPorId = async (req, res) => {
     try {
-        const id = Number(req.params.id);
+      const id = Number(req.params.id);
 
-        const response = await EstacaoService.listarPorID({ id });
-        return sendResponse(res, 200, {
-            data: response
-        });
+      const response = await EstacaoService.listarPorID({ id });
+      return sendResponse(res, 200, {
+        data: response
+      });
 
     } catch (error) {
-        if (error instanceof APIErro) {
-            const { code, errors } = error.toJson();
-            return sendError(res, code, ...errors);
-        }
+      if (error instanceof APIErro) {
+        const { code, errors } = error.toJson();
+        return sendError(res, code, ...errors);
+      }
 
-        if (error instanceof z.ZodError) {
-            let errors = [];
-            error.issues.map((issue) => (
-                errors.push({
-                    path: issue.path[0],
-                    message: issue.message
-                })
-            ));
-            return sendError(res, 400, errors);
-        }
+      if (error instanceof z.ZodError) {
+        let errors = [];
+        error.issues.map((issue) => (
+          errors.push({
+            path: issue.path[0],
+            message: issue.message
+          })
+        ));
+        return sendError(res, 400, errors);
+      }
 
-        sendError(res, 500, []);
+      sendError(res, 500, []);
     }
-};
+  };
 
 
 
@@ -93,14 +98,14 @@ class Estacao {
   static cadastrar = async (req, res) => {
     try {
       const response = await EstacaoService.inserir(req.body);
-      return sendResponse(res,201,{
+      return sendResponse(res, 201, {
         data: response,
 
       });
-  
+
     } catch (error) {
-      if(error.code && error.error){
-        return sendError(res,error.code, [error.error]);
+      if (error.code && error.error) {
+        return sendError(res, error.code, [error.error]);
       }
       if (error instanceof z.ZodError) {
         let errors = [];
@@ -112,10 +117,10 @@ class Estacao {
             }
           );
         });
-        return sendError(res,400,errors);
-        
+        return sendError(res, 400, errors);
+
       }
-      return sendError(res,500,[]);
+      return sendError(res, 500, []);
     };
   };
 
