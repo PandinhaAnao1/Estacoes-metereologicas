35a86fd258d6987c7873e85c153af6d82ffdeee2
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2024-11-12 16:58:17 -0400

    Inserir o comun response na documentacao de listar estações fala corrigir alguns elementos

--- src/docs/routes/estacao/estacaoListar.js
+++ src/docs/routes/estacao/estacaoListar.js
@@ -1,3 +1,5 @@
+import commonResponses from "../../schemas/commonResponses.js";
+
 const estacaoListar = {
   "/estacoes/": {
     get: {
@@ -84,49 +86,7 @@ const estacaoListar = {
         }
       ],
       responses: {
-        200: {
-          content: {
-            "application/json": {
-              schema: {
-                type: "object",
-                properties: {
-                  data: {
-                    type: "object", items: {}, example:
-                    {
-                      data: {
-                        type: 'array', example: [
-                          {
-                            "id": 1,
-                            "nome": "Estação Central",
-                            "endereco": "Rua Exemplo, 123",
-                            "latitude": "11.1111",
-                            "longitude": "22.2222",
-                            "ip": "192.168.1.1",
-                            "status": "ativo",
-                            "usuario_id": 1
-                          },
-                          {
-                            "id": 2,
-                            "nome": "Estação Norte",
-                            "endereco": "Avenida Exemplo, 456",
-                            "latitude": "33.3333",
-                            "longitude": "44.4444",
-                            "ip": "192.168.1.2",
-                            "status": "inativo",
-                            "usuario_id": 2
-                          }
-                        ]
-                      },
-                      error: { type: "boolean", example: false },
-                      code: { type: "int", example: 200 },
-                      message: { type: "string", example: "Estações encontradas com sucesso" }
-                    }
-                  },
-                }
-              }
-            }
-          }
-        },
+        200: commonResponses["200"]("#/components/schemas/estacoes_listar_200"),
         400: {
           content: {
             "application/json": {
---
+++ src/docs/schemas/estacoes/estacoesListar.js
@@ -0,0 +1,120 @@
+const estacoesListarSchemas = {
+
+    estacoes_listar_200: {
+        type: "object",
+        properties: {
+            data: {
+                type: "object", items: {}, example:
+                {
+                    data: {
+                        type: 'array', example: [
+                            {
+                                "id": 1,
+                                "nome": "Estação Central",
+                                "endereco": "Rua Exemplo, 123",
+                                "latitude": "11.1111",
+                                "longitude": "22.2222",
+                                "ip": "192.168.1.1",
+                                "status": "ativo",
+                                "usuario_id": 1
+                            },
+                            {
+                                "id": 2,
+                                "nome": "Estação Norte",
+                                "endereco": "Avenida Exemplo, 456",
+                                "latitude": "33.3333",
+                                "longitude": "44.4444",
+                                "ip": "192.168.1.2",
+                                "status": "inativo",
+                                "usuario_id": 2
+                            }
+                        ]
+                    },
+                    error: { type: "boolean", example: false },
+                    code: { type: "int", example: 200 },
+                    message: { type: "string", example: "Estações encontradas com sucesso" }
+                }
+            },
+        },
+    },
+    estacoes_cadastrar_400: {
+
+        properties: {
+            data: {
+                description: "Obejto resultado, cadastrado no banco de dados.",
+                type: "array",
+                example: []
+            },
+            code: {
+                type: "int",
+                example: 201,
+                description: "Codigo http da requisição.",
+
+            },
+            message: {
+                description: "Campo que mostra uma mensagem de resultado da requisição.",
+                type: "string",
+                example: "Requisição com sintaxe incorreta ou outros problemas!",
+            },
+            error: { type: "boolean", example: true },
+            errors: {
+                type: "array", example:
+                    [
+                        {
+                            path: "nome",
+                            message: "Nome é obrigatório."
+                        },
+                        {
+                            path: "endereco",
+                            message: "Email é obrigatório."
+                        },
+                        {
+                            path: "latitude",
+                            message: "Latitude informada não é do tipo number."
+                        },
+                        {
+                            path: "longitude",
+                            message: "Longitude informada não é do tipo number."
+                        },
+                        {
+                            path: "ip",
+                            message: "Ip é obrigatório."
+                        },
+                        {
+                            path: "status",
+                            message: "Status informado não corresponde ao formato indicado (ativo ou inativo)."
+                        },
+                        {
+                            path: "usuario_id",
+                            message: "Estação sem vínculo com usuário."
+                        }
+                    ]
+            }
+        },
+
+    },
+    estacoes_cadastrar_500: {
+        properties: {
+            data: {
+                description: "Obejto resultado, cadastrado no banco de dados.",
+                type: "array",
+                example: []
+            },
+            code: {
+                type: "int",
+                example: 500,
+                description: "Codigo http da requisição.",
+
+            },
+            message: {
+                description: "Campo que mostra uma mensagem de resultado da requisição.",
+                type: "string",
+                example: "Requisição com sintaxe incorreta ou outros problemas!",
+            },
+            error: { type: "boolean", example: true },
+            errors: { type: "array", example: [] }
+        },
+
+    },
+};
+export default estacoesListarSchemas;
