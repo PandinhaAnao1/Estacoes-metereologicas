573fe9e1b5e40d77c7daf3c88c3d8335848d56da
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2024-11-12 10:51:24 -0400

    Corrigir o teste de unidade de cadastrar do controller para obter o erro 400

--- src/tests/unitario/controller/estacaoController.spec.js
+++ src/tests/unitario/controller/estacaoController.spec.js
@@ -2,6 +2,7 @@ import { json } from 'express';
 import EstacaoController from '../../../controllers/estacaoController.js';
 import EstacaoService from '../../../services/estacaoService.js';
 import { describe, expect } from '@jest/globals';
+import { APIErro } from '../../../util/apiErrro.js';
 
 jest.mock('../../../services/usuarioService', () => ({
     listar: jest.fn().mockRejectedValue(new Error('Erro interno do serviço')),
@@ -12,7 +13,9 @@ jest.mock('../../../services/usuarioService', () => ({
 ;
 
 jest.mock('../../../services/estacaoService.js', () => ({
-    listar: jest.fn(),  
+    listar: jest.fn(),
+    inserir: jest.fn(),  
+
 }));
 
 
@@ -27,23 +30,16 @@ describe('Teste que verifica o controller de estações', () => {
     let mockResp;
 
     beforeEach(() => {
-        mockReq = {
+        req = {
             params: {},
             body: {},
             query: {}
-          };
-        mockResp = {
-            header: jest.fn().mockReturnThis(), 
-            status: jest.fn().mockReturnThis(), 
-            send: jest.fn(), 
-            json: jest.fn()
-        };
-        req = {
-            body: {}
         };
 
         res = {
-            status: jest.fn().mockReturnThis(),
+            header: jest.fn().mockReturnThis(), 
+            status: jest.fn().mockReturnThis(), 
+            send: jest.fn(), 
             json: jest.fn()
         };
     });
@@ -53,11 +49,10 @@ describe('Teste que verifica o controller de estações', () => {
         it('Deve retornar erro 500 ao listar estações', async () => {
             EstacaoService.listar.mockRejectedValue(new Error('Erro interno do serviço'));
    
-            await EstacaoController.listar(mockReq, mockResp);
+            await EstacaoController.listar(req, res);
             
-            console.log(mockResp);
             
-            expect(mockResp.status).toHaveBeenCalledWith(500);
+            expect(res.status).toHaveBeenCalledWith(500);
 
         });
         it('Deve retornar erro 500 ao listar estação por ID', async () => {
@@ -74,10 +69,8 @@ describe('Teste que verifica o controller de estações', () => {
     });
     describe('Teste de cadastrar controller estações.', () => {
         it('Deve retornar erro 400 ao cadastrar uma estação', async () => {
-            const sendErrorMock = jest.fn();
-            const res = { status: jest.fn(() => ({ json: sendErrorMock })) };
 
-            const req = { body: null };
+            EstacaoService.inserir.mockRejectedValue({ error: 'Erro ao cadastrar estação', code: 400 });
 
             await EstacaoController.cadastrar(req, res);
 
