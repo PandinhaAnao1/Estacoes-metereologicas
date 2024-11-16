4d7e167a18905a2c054223223d18b87730fa1ab7
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2024-11-15 20:46:10 -0400

    Corrigir a documentação da rota de cadastrar usuarios

--- src/docs/schemas/usuarios/usuarioCadastrar.js
+++ src/docs/schemas/usuarios/usuarioCadastrar.js
@@ -1,59 +1,28 @@
 const cadastrarSchemas = {
-    
     usuario_cadastrar_201: {
         type: "object",
-        properties: {
-            data: {
-                type: "object",
-                example: {
-                  id: 1,
-                  nome: "marcos",
-                  email: "1marcos@gmail.com",
-                }
-            },
-            error: {
-                type: "boolean",
-                example: false
-            },
-            code: {
-                type: "integer",
-                example: 201
-            },
-            message: {
-                type: "string",
-                example: "Usuário cadastrado com sucesso!"
-            }
+        example: {
+            id: 1,
+            nome: "marcos",
+            email: "1marcos@gmail.com",
         }
     },
-
     usuario_cadastrar_400: {
-        properties: {
-            error: { 
-                type: "boolean", 
-                example: true 
+        type: "array",
+        example: [
+            {
+                message: "Formato de email inválido",
+                path: "email"
             },
-            code: { 
-                type: "int", 
-                example: 400 
+            {
+                message: "Nome é obrigatório",
+                path: "nome"
             },
-            errors: { 
-                type: "array", 
-                example: 
-                [
-                    "Campo Nome É Obrigatório!", 
-                    "O nome tem que ser String", 
-                    "O Nome Deve Conter Pelo Menos 3 Letras!", 
-                    "Campo Email É Obrigatório!", 
-                    "O Email Tem Que Ser String", 
-                    "Email Invalido!", 
-                    "Campo Senha É Obrigatório!", 
-                    "O Senha Tem Que Ser String", 
-                    "A senha deve conter pelo menos uma letra minúscula, uma letra maiúscula, um número e um símbolo.", 
-                    "Email Já Cadastrado!"
-                ] 
+            {
+                message: "A senha deve ter pelo menos 6 caracteres",
+                path: "senha"
             }
-
-        }
+        ]
     }
 }
 
